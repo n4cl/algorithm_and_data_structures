2c5ec4e34f0c9a0642c89373cb4218616a3109d3
n4cl <devn4cl@gmail.com>
2017-06-20 00:55:59 +0900

    Updated ALDS1_1_D.py to be AC

--- ALDS1_1_D.py
+++ ALDS1_1_D.py
@@ -5,12 +5,11 @@ r = [input() for _ in xrange(n)]
 l = len(r)
 
 max_v = - 10 ** 9
+min_v = r[0]
 
-# 入力した順に差を求め、差の最大値を得る
-for i in xrange(l - 1):
-    for j in xrange(i + 1, l):
-        diff = r[j] - r[i]
-        if diff > max_v:
-            max_v = diff
+# 最大値と最小値の差を得る
+for i in xrange(1, l):
+    max_v = max(max_v, r[i] - min_v)
+    min_v = min(min_v, r[i])
 
 print max_v
