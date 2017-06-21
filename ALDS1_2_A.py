4480fe394569f867e884e1dfee636325afbeda4e
n4cl <devn4cl@gmail.com>
2017-06-22 00:12:00 +0900

    Add ALDS1_2_A.py

---
+++ ALDS1_2_A.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+
+def bubble_sort(a, n):
+    # バブルソート
+
+    cnt = 0
+    flag = 1
+
+    while flag:
+        flag = 0
+
+        for j in xrange(n-1, 0, -1):
+            if a[j] < a[j-1]:
+                a[j], a[j-1] = a[j-1], a[j]
+                flag = 1
+                cnt += 1
+
+    # ソート結果
+    print " ".join(map(str, a))
+    print cnt
+
+
+if __name__ == '__main__':
+
+    i_n = input()
+    i_a = map(int, raw_input().split())
+
+    bubble_sort(i_a, i_n)
