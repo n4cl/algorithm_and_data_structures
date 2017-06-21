3282b0df02f8eb9229a4bd69551b2000ff49789a
n4cl <devn4cl@gmail.com>
2017-06-22 00:19:09 +0900

    Update ALDS1_2_A.py to have unsorted index position

--- ALDS1_2_A.py
+++ ALDS1_2_A.py
@@ -6,15 +6,18 @@ def bubble_sort(a, n):
 
     cnt = 0
     flag = 1
+    i = 0
 
     while flag:
         flag = 0
 
-        for j in xrange(n-1, 0, -1):
+        for j in xrange(n-1, i, -1):
             if a[j] < a[j-1]:
                 a[j], a[j-1] = a[j-1], a[j]
                 flag = 1
                 cnt += 1
+
+        i += 1
 
     # ソート結果
     print " ".join(map(str, a))
